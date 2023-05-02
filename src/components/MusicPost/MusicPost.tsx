f869a4be18fc3425f5a4a54a0676bc9dbf35d376
Arcades2 <peret.etienne@gmail.com>
2023-05-02 22:05:50 +0200

    All players stop when a player start

--- src/components/MusicPost/MusicPost.tsx
+++ src/components/MusicPost/MusicPost.tsx
@@ -1,6 +1,8 @@
+import React, { useEffect } from "react";
 import dynamic from "next/dynamic";
 import { type RouterOutputs } from "~/utils/api";
 import Avatar from "~/components/Avatar";
+import { usePlayersActions, usePlayerState } from "~/stores/players";
 
 const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
@@ -8,21 +10,42 @@ type MusicPostProps = {
   post: RouterOutputs["post"]["getFollowingPosts"][number];
 };
 
-const MusicPost = ({ post }: MusicPostProps) => (
-  <div className="flex flex-col gap-4 p-2">
-    <Avatar user={post.user} />
-    <p>{post.createdAt.toLocaleDateString("fr-fr")}</p>
-    <p>{post.description}</p>
-    <ReactPlayer
-      url={post.url}
-      controls
-      width="100%"
-      style={{
-        borderRadius: "0.5rem",
-        overflow: "hidden",
-      }}
-    />
-  </div>
-);
+const MusicPost = ({ post }: MusicPostProps) => {
+  const playerState = usePlayerState(post.id);
+  const playersActions = usePlayersActions();
+
+  useEffect(() => {
+    playersActions.addPlayer(post.id);
+
+    return () => {
+      playersActions.removePlayer(post.id);
+    };
+  }, [playersActions, post.id]);
+
+  return (
+    <div className="flex flex-col gap-4 p-2">
+      <Avatar user={post.user} />
+      <p>{post.createdAt.toLocaleDateString("fr-fr")}</p>
+      <p>{post.description}</p>
+      <ReactPlayer
+        light
+        url={post.url}
+        controls
+        width="100%"
+        playing={playerState.playing}
+        style={{
+          borderRadius: "0.5rem",
+          overflow: "hidden",
+        }}
+        onPlay={() => {
+          playersActions.playPlayer(post.id);
+        }}
+        onPause={() => {
+          playersActions.pausePlayer(post.id);
+        }}
+      />
+    </div>
+  );
+};
 
 export default MusicPost;
