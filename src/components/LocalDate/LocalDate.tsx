137157101b24346548ddbf7dedf02c2e9c33326d
Arcades2 <peret.etienne@gmail.com>
2023-09-12 12:24:07 +0200

    "x time ago" in post date

--- src/components/LocalDate/LocalDate.tsx
+++ src/components/LocalDate/LocalDate.tsx
@@ -1,13 +1,10 @@
+import { getTimeAgoOrExactDate } from "~/utils/date";
+
 export type LocalDateProps = {
   date: Date;
   className?: string;
 };
 
-// TODO : code "x minutes/hours ago" conditions + dynamic localization
 export default function LocalDate({ date, className }: LocalDateProps) {
-  return (
-    <div className={className}>
-      {date.toLocaleDateString("fr-fr")} - {date.toLocaleTimeString("fr-fr")}
-    </div>
-  );
+  return <div className={className}>{getTimeAgoOrExactDate(date)}</div>;
 }
